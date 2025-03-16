3a824282f56de4cb63a4135bd0791fb188414ac7
ibahmman <69468333+ibahmman@users.noreply.github.com>
2025-03-16 20:25:31 +0330

    Delete drfiransms directory

--- drfiransms/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrfiransmsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'drfiransms'
-
-    def ready(self):
-        import drfiransms.signals
--- drfiransms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import OTPCodeSend ,Authentication
-
-urlpatterns = [
-    path('send/otpcode/', OTPCodeSend.as_view()),
-    path('auth/', Authentication.as_view())
-]
